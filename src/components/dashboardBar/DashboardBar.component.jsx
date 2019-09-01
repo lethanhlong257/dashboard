201a239e88a141e34f6073dff2916c37b7d6aef4
longle <>
2019-09-02 00:40:00 +0700

    Dashboard bar component

--- src/components/dashboardBar/DashboardBar.component.jsx
+++ src/components/dashboardBar/DashboardBar.component.jsx
@@ -1,38 +1,28 @@
 import React, { Component } from 'react';
-import { Navbar, NavDropdown, Breadcrumb} from 'react-bootstrap';
+import { Navbar, NavDropdown, Container } from 'react-bootstrap';
+
+import "./DashboardBar.style.scss"
 
 export default class DashboardBar extends Component {
   render() {
     return (
-      <div className="dashboard-bar">
-        <Navbar bg="dark" expand="lg">
-          <Navbar.Brand href="#home" className="mr-auto">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
+      <Navbar bg="dark" expand="lg" className="dashboard-bar">
+        <Container>
+          <Navbar.Brand href="#home" className="mr-auto dashboard-bar__brand">
+            <i className="fas fa-columns"></i>
             {' Dashboard'}
           </Navbar.Brand>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <i className="far fa-user dashboard-bar__icon"></i>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown" className="dashboard-bar__text">
+            <NavDropdown.Item href="#action/3.1" className="dashboard-bar--black-text">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2" className="dashboard-bar--black-text">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3"className="dashboard-bar--black-text">Something</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4" className="dashboard-bar--black-text">Separated link</NavDropdown.Item>
           </NavDropdown>
-        </Navbar>
+        </Container>
 
-        <Breadcrumb>
-          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-          <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-            Library
-          </Breadcrumb.Item>
-          <Breadcrumb.Item active>Data</Breadcrumb.Item>
-        </Breadcrumb>
-      </div>
+      </Navbar>
     )
   }
 }
